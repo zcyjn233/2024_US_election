62c99ffedf0d2339c2ca6d777a23b2f794e8809e
zcyjn233 <won.wang@mail.utoronto.ca>
2024-11-04 15:57:29 -0500

    Update 06-model_data.R

--- scripts/06-model_data.R
+++ scripts/06-model_data.R
@@ -1,37 +1,52 @@
-#### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+
+# Purpose: First model
+# Author: Wei Wang , Chiyue Zhuang
+# Date: 4 November 2024
+# Contact: won.wang@mail.utoronto.ca, chiyue.zhuang@mail.utoronto.ca
 
 
 #### Workspace setup ####
+library(plyr)
+library(ggiraph)
+library(ggiraphExtra)
 library(tidyverse)
 library(rstanarm)
-
+library(ggplot2)
+library(arrow)
+library(broom)
 #### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+data1 <- read_parquet("/Users/lucky/Desktop/paper-2/data/02-analysis_data/DT.parquet")
 
-### Model data ####
-first_model <-
-  stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
-    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 853
-  )
+head(data1)
+# Convert 'pollster' and 'state' to factors, and 'end_date' to Date format
+data1$pollster <- as.factor(data1$pollster)
+data1$state <- as.factor(data1$state)
+data1$end_date <- as.Date(data1$end_date, format = "%Y-%m-%d") # Adjust the date format if necessary
+data1$pollscore <- as.numeric(as.character(data1$pollscore))
+data1$pct <- as.numeric(as.character(data1$pct))
 
+# Handle any NA values by filling or removing as needed
+# data1$pollscore[is.na(data1$pollscore)] <- mean(data1$pollscore, na.rm = TRUE)
+# data1$pct[is.na(data1$pct)] <- mean(data1$pct, na.rm = TRUE)
+# data1 <- na.omit(data1)
+# Fit the linear model
+str(data1)
+View(data1)
+str(data1[,c("pct","race_id","pollster")])
+numeric_vars <- names(data1)[sapply(data1, is.numeric)]
+print(numeric_vars)
+paste0(numeric_vars,collapse ="+")
+view(data1[,numeric_vars])
+data1=data1[numeric_vars]
+view(data1)
+linear.model2 <- lm(pct ~ numeric_grade+pollscore+
+                      transparency_score , data = data1)
+# Display the summary and confidence intervals of the model
+summary(linear.model2)
+confint(linear.model2)
+# Load ggiraphExtra for visualization
+if (!require("ggiraphExtra")) install.packages("ggiraphExtra")
+ggPredict(linear.model2, digits = 1, show.point = TRUE, se = TRUE, xpos = 0.5)
 
 #### Save model ####
-saveRDS(
-  first_model,
-  file = "models/first_model.rds"
-)
-
-
+saveRDS(linear.model2,"/Users/lucky/Desktop/paper-2/models/linear.model2.rds")
