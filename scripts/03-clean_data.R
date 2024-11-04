a843cec7d3093c02de77969babcb198833a94bce
zcyjn233 <won.wang@mail.utoronto.ca>
2024-11-04 15:57:51 -0500

    Update 03-clean_data.R

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -1,44 +1,65 @@
-#### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+
+# Purpose: Cleans the raw data
+# Author: Wei Wang , Chiyue Zhuang
+# Date: 4 November 2024
+# Contact: won.wang@mail.utoronto.ca, chiyue.zhuang@mail.utoronto.ca
 
 #### Workspace setup ####
 library(tidyverse)
-
+library(arrow)
+library(janitor)
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+data <- read_csv("/Users/lucky/Desktop/paper-2/data/01-raw_data/president_polls.csv") |>
+  clean_names()
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
+just_trump_high_quality <- data |>
+  filter(
+    candidate_name == "Donald Trump",
+    numeric_grade >= 2.8 # Need to investigate this choice - come back and fix.
+    # Also need to look at whether the pollster has multiple polls or just one or two - filter out later
   ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
   mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
+    state = if_else(is.na(state), "National", state), # Hacky fix for national polls - come back and check
+    end_date = mdy(end_date)
   ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+  filter(end_date >= as.Date("2024-07-02")) |> 
+  mutate(
+    num_trump = round((pct / 100) * sample_size, 0) # Need number not percent for some models
+  )
+#### Plot data ####
+base_plot <- ggplot(just_trump_high_quality, aes(x = end_date, y = pct)) +
+  theme_classic() +
+  labs(y = "Trump percent", x = "Date")
+# Plots poll estimates and overall smoothing
+base_plot +
+  geom_point() +
+  geom_smooth()
+# Color by pollster
+# This gets messy - need to add a filter - see line 21
+base_plot +
+  geom_point(aes(color = pollster)) +
+  geom_smooth() +
+  theme(legend.position = "bottom")
+# Facet by pollster
+# Make the line 21 issue obvious
+# Also - is there duplication???? Need to go back and check
+base_plot +
+  geom_point() +
+  geom_smooth() +
+  facet_wrap(vars(pollster))
+# Color by pollscore
+base_plot +
+  geom_point(aes(color = factor(pollscore))) +
+  geom_smooth() +
+  theme(legend.position = "bottom")
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_parquet(x = just_trump_high_quality,
+              sink = "/Users/lucky/Desktop/paper-2/data/02-analysis_data/DT.parquet")
+
+# 读取Parquet文件
+df <- read_parquet("/Users/lucky/Desktop/paper-2/data/02-analysis_data/DT.parquet")
+
+# 保存为CSV文件
+write.csv(df, "/Users/lucky/Desktop/paper-2/data/02-analysis_data/DT.csv", row.names = FALSE)
+
